be834ead985dc76d6ba2fccdc50818897c652411
SoulReaper496 <praveendasking@gmail.com>
2021-09-06 01:34:42 +0530

    arrayfunction

--- Day1/array.js
+++ Day1/array.js
@@ -35,7 +35,7 @@ const companies = [
       console.log(company.start);
   })
 
-  //filter 
+ // filter 
 
       const canGo = [];
       for (let i = 0; i < ages.length; i++){
@@ -45,6 +45,13 @@ const companies = [
       }
    
     console.log(canGo);
+
+    //filter methods for same code
+
+    const canDrink = ages.filter( age => age >=21);
+       
+        console.log(canDrink);
+    
     //find a smallest
   var smallest = canGo[0];
   for(var i=1; i<canGo.length; i++){
@@ -53,10 +60,80 @@ const companies = [
       }
       console.log(smallest);
   }
-  //using short to find max and min
+  //using short to find max
   let a =canGo.sort((a,b) => a-b)
   const max = a[a.length-1];
  // console.log(a);
   console.log(a);
   console.log (max);
     
+  //using filter to identify specific string match
+
+  const str = companies.filter(company => company.category === "Retail");
+  console.log (str);
+
+  //map
+
+  const test = companies.map ( company  => `${company.name} [${company.start} - ${company.end}]` );
+  console.log(test);
+
+  //map with numbers
+
+//   const squreno = ages.map (age => Math.sqrt(age));
+//   console.log (squreno);
+
+//using multiple maps 
+ 
+const squreno = ages.
+map (age => Math.sqrt(age)).
+map(age => age *2);
+ console.log (squreno);
+
+
+ //sSORT
+// sort method using normal function..
+ const startcmpny = companies.sort(function (a,b){
+     if (a.start > b. start){
+         return 1;
+     }else {
+         return -1;
+     }
+ })
+
+
+ // sort using arrow functions
+
+ const startofcompanies = companies .sort((a,b)=> a.end < b.end ? 1 : -1);
+ console.log(startofcompanies)
+
+ //reduce 
+
+ //using normal function
+    
+//  const addAges = ages.reduce(function (total, age){
+//       return total + age;
+// });
+// console.log (addAges);
+
+//using Arrow functions
+
+const addAges = ages.reduce( (total, age) =>
+     total + age);
+console.log (addAges);
+
+//getting total timeperiod of companies 
+
+const totalYears = companies.reduce(function (total, company){
+    return total + (company.end - company.start);
+},0);
+console.log(totalYears);
+
+//combined functions of ages
+
+ const combine = ages 
+ .map (age => age *2)
+ .filter(age => age>40)
+ .sort((a,b) => b -a)
+// .reduce ((total,b) => total + b, 0)
+
+ console.log(combine);
